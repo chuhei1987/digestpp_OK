3bc68118b1caabc22ac5c7bc9818933b19d07c96
CHUHEI <5563338+chuhei1987@users.noreply.github.com>
2023-09-08 15:57:27 +0800

    Creating something useful
    Creating something useful after reading things in test.
    
    In here, I provide two things.
    
    A simple box which acts like what you see in *nix.
    
    a Certutil-ish like box , 9 strong methods during the writing times.
    
    I was finding ways how to reduce the size, unfortunately, just what the author says, Only tested with VC++ and clang.

---
+++ operation/sumwhirlpool.cc
@@ -0,0 +1,55 @@
+/****************************************************************
+*																*
+*sumwhirlpool.cc  												*
+*Public Domain													*
+*																*
+*ISO/IEC 10118-3:2018									 		*
+*												 				*
+*CHUHEI														    *
+*created on 2023-09-08											*
+****************************************************************/
+
+
+#include "../digestpp.hpp"
+#include <numeric>
+#include <iostream>
+#include <iomanip>
+#include <fstream>
+
+using namespace std;
+using namespace digestpp;
+
+
+int main(int argc, char *argv[])
+{
+	
+	/*
+	This requires digestpp;
+	if just typing the exe just output how to use that
+	The filename just like *nix style
+	If input es deux then check if file exist
+	Not exist? Then Send back to the start
+	Else
+	Read file in binary mode, checking the hash and that's it.
+	*/
+	
+	/*How fast? Golang requires Interpreter to do a thing while c++ may point something deeper */
+	
+	if (argc != 2){
+	cout << "File Integrity checker powered by digestpp" << endl;
+	cout << "Usage: sumwhirlpool (filename) " << endl;
+	
+	}
+	else{
+	ifstream file(argv[1], ios_base::in|ios_base::binary);
+	
+	
+	if (!file)
+	{ /*File is empty*/
+	cout << "File not found, please double check the correct location trying again." << endl;
+	}
+	else{
+	cout <<whirlpool().absorb(file).hexdigest() << endl;
+	}
+	}
+}
